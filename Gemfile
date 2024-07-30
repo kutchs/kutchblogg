0b50e4ccdab14d2a655bcbbb6a3e025c4fc802eb
elodie moraud <elodie@kutchs.fr>
2024-07-30 15:52:39 +0200

    Set up dotenv

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem 'faker'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
@@ -52,9 +54,11 @@ gem "simple_form", github: "heartcombo/simple_form"
 gem "sassc-rails"
 
 group :development, :test do
-  gem "dotenv-rails"
+  # gem "dotenv-rails"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mswin mswin64 mingw x64_mingw ]
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+	gem 'dotenv-rails', groups: [:development, :test]
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,55 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+User.destroy_all
+Category.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password',
+    nickname: Faker::Internet.username(specifier: 3..8)
+  )
+end
+
+
+5.times do
+  Category.create!(
+    name: Faker::Book.genre
+  )
+end
+
+
+# Create Posts
+users = User.all
+categories = Category.all
+
+20.times do
+  Post.create!(
+    user: users.sample,
+    category: categories.sample,
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 5),
+    url: Faker::Internet.url,
+    rating: rand(1.0..5.0).round(1),
+    date: Faker::Date.backward(days: 30)
+  )
+end
+
+# Create Comments
+posts = Post.all
+
+50.times do
+  Comment.create!(
+    user: users.sample,
+    post: posts.sample,
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
+
+puts "Seed data created successfully!"

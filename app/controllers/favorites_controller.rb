e3aaf69a4f74d8cece49c32b20da9f3c5277345d
elodie moraud <elodie@kutchs.fr>
2024-07-30 20:46:33 +0200

    ok

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -13,7 +13,7 @@ class FavoritesController < ApplicationController
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post: post)
-    favorite.destroy
+    favorite.destroy if favorite
     respond_to do |format|
       format.html { redirect_to posts_path, notice: "Removed from favorites." }
       format.turbo_stream { render turbo_stream: turbo_stream.replace("favorite_#{post.id}", partial: "posts/favorite", locals: { post: post }) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
 
 
   def favorited?(post)
-    favorited_posts.include?(post)
+    favorites.exists?(post_id: post.id)
   end
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

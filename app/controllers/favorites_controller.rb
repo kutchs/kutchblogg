f09eeff968889a15469d4b0ddea175d575b06a26
elodie moraud <elodie@kutchs.fr>
2024-07-31 08:30:56 +0200

    ok

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,7 +7,7 @@ class FavoritesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to posts_path, notice: "Added to favorites." }
       format.turbo_stream { render turbo_stream: turbo_stream.replace("favorite_#{post.id}", partial: "posts/favorite", locals: { post: post }) }
-    end
+    # Remove the extra 'end' statement
   end
 
   def destroy
@@ -18,5 +18,12 @@ class FavoritesController < ApplicationController
       format.html { redirect_to posts_path, notice: "Removed from favorites." }
       format.turbo_stream { render turbo_stream: turbo_stream.replace("favorite_#{post.id}", partial: "posts/favorite", locals: { post: post }) }
     end
+  end
+end
+
+private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
